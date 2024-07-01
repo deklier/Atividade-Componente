051b423be674d9c6f3f3b7c891613bc9cdc3a915
deklier <deklier@gmail.com>
2024-07-01 17:18:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Componente from './Componente';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World!</h1>
+        <p1>Meu segundo Hello World!</p1>
+        <Componente />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componente.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Componente() {
+  return (
+    <div>
+      <h2>Segundo Componente do Hello World!</h2>
+      <p>Conteúdo do componente secundário.</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Componente;
